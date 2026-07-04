cf343cc93402d81d8bf471bad1235e2e51bad24a
amitbishnoii <amitbishnoi29012@gmail.com>
2026-07-04 23:25:18 +0530

    finish v1 of portfolio

--- src/data/navItems.js
+++ src/data/navItems.js
@@ -1,7 +1,7 @@
 export const navItems = [
     { name: "Home", to: "/" },
     { name: "Projects", to: "/projects" },
-    { name: "Github", to: "/github" },
-    { name: "LeetCode", to: "/leetcode" },
+    { name: "Github", to: "https://github.com/amitbishnoii/" },
+    { name: "LeetCode", to: "https://leetcode.com/u/aMitBishnoiiiiii/" },
     { name: "Contact", to: "/contact" },
 ];
